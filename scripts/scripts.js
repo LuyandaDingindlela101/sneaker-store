099c87c628f81c021ebfbb602fbbfc8357989e3d
LuyandaGitHub <49154286+LuyandaGitHub@users.noreply.github.com>
2021-05-04 22:24:48 +0200

    worked on a couple sections. contact section and animations left with some tweeks

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,27 +2,27 @@
 
 // Initialize fullpage Fullpage
 new fullpage("#fullpage", {
-  anchors: ["home", "products", "reviews", "contact"],
-  navigation: true,
-  navigationPosition: "left",
-  navigationTooltips: ["Home", "Products", "Reviews", "Contact"],
-  center: true,
+	anchors: ["home", "products", "reviews", "contact"],
+	navigation: true,
+	navigationPosition: "left",
+	navigationTooltips: ["Home", "Products", "Reviews", "Contact"],
+	center: true,
 });
 
 //   Init Animate On Scroll
 AOS.init();
 
 $(document).ready(function () {
-  //   Initialize product slider and set options
-  $("#product-slider").owlCarousel({
-    items: 1,
-    margin: 0,
-    stagePadding: 200,
-    loop: true,
-    dots: false,
-    //   autoplay: true,
-    center: true,
-  });
+	//   Initialize product slider and set options
+	$("#product-slider").owlCarousel({
+		items: 1,
+		margin: 0,
+		stagePadding: 200,
+		loop: true,
+		dots: false,
+		// autoplay: true,
+		center: true
+	});
 });
 
 //   function to open modal
